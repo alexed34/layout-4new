272812ba0ce466bb4a588e83c746928584601511
Эдуард <44254815+alexed34@users.noreply.github.com>
2020-06-01 16:44:29 +0400

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ rendered_page = template.render(
 with open('index.html', 'w', encoding="utf8") as file:
     file.write(rendered_page)
 
-server = HTTPServer(('localhost', 8000), SimpleHTTPRequestHandler)
+server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
 
 server.serve_forever()
 
